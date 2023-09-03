082443e6824b3c32995e542285ae8dc65b4420b7
Issam_lizame <106539985+Issamssm@users.noreply.github.com>
2023-09-04 00:37:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ const Home = () => {
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
         </div>
-        {/* <ParticlesContainer /> */}
+        <ParticlesContainer />
         <motion.div
           className=" lg:bottom-0 lg:right-[8%] w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32"
           variants={fadeIn('up', 0.5)}
